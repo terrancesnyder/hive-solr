37ab3982d11cb556d024be2bd87c445d8797fa96
Terrance A. Snyder <terrance.a.snyder@gmail.com>
2015-03-03 10:46:48 -0500

    [simpler] solr doesn't care about physical type so just check for null and insert - schema defines format

--- src/main/java/com/chimpler/hive/solr/SolrWriter.java
+++ src/main/java/com/chimpler/hive/solr/SolrWriter.java
@@ -42,66 +42,30 @@ public class SolrWriter implements RecordWriter {
 		SolrInputDocument doc = new SolrInputDocument();
 		for (final Map.Entry<Writable, Writable> entry : map.entrySet()) {
 			String key = entry.getKey().toString();
-			Object val = getObjectFromWritable(entry.getValue());
+			String token = getStringValue(entry.getValue());
 			// skip null values
-			if (val == null) {
+			if (token == null) {
 				continue;
 			}
-			// check if this is a multi-value field
-			if (val instanceof String) {
-				String token = ((String)val).trim();
-				// is this string in hives array format '["0","1","2","3"]'
-				if (token.contains(",") && token.startsWith("[") && token.endsWith("]")) {
-					String[] items = token.replace("[", "").replace("]", "").replace("\"", "").replace("'", "").split(",");
-					for (String item : items) {
-						doc.addField(key, item.trim());
-					}
-				} else {
-					// single value field
-					doc.setField(key, val);
+			// is this string in hives array format '[0,1,2,3]'
+			if (token.contains(",") && token.startsWith("[") && token.endsWith("]")) {
+				String[] items = token.replace("[", "").replace("]", "").replace("\"", "").replace("'", "").split(",");
+				for (String item : items) {
+					doc.addField(key, item.trim());
 				}
 			} else {
-				doc.setField(key, val);
+				// single value field
+				doc.setField(key, token);
 			}
 		}
 		table.save(doc);
 	}
 
-	private Object getObjectFromWritable(Writable w) {
+	private String getStringValue(Writable w) {
 		if (w == null) {
 			return null;
 		}
-		try {
-			if (w instanceof IntWritable) {
-				// int
-				return ((IntWritable) w).get();
-			} else if (w instanceof ShortWritable) {
-				// short
-				return ((ShortWritable) w).get();
-			} else if (w instanceof ByteWritable) {
-				// byte
-				return ((ByteWritable) w).get();
-			} else if (w instanceof BooleanWritable) {
-				// boolean
-				return ((BooleanWritable) w).get();
-			} else if (w instanceof LongWritable) {
-				// long
-				return ((LongWritable) w).get();
-			} else if (w instanceof FloatWritable) {
-				// float
-				return ((FloatWritable) w).get();
-			} else if (w instanceof DoubleWritable) {
-				// double
-				return ((DoubleWritable) w).get();
-			}else if (w instanceof NullWritable) {
-				//null
-				return null;
-			} else {
-				// treat as string
-				return w.toString();
-			}
-		} catch (Exception ex) {
-			return w.toString();
-		}
+		// treat as string
+		return w.toString();
 	}
 }
