46af1e04e508b76deab9b035d87e8b7e2c1872d4
Terrance A. Snyder <terrance.a.snyder@gmail.com>
2018-05-09 19:49:23 +0900

    [fix]

--- src/main/java/com/chimpler/hive/solr/SolrReader.java
+++ src/main/java/com/chimpler/hive/solr/SolrReader.java
@@ -12,63 +12,62 @@ import org.apache.solr.common.SolrDocument;
 
 public class SolrReader implements RecordReader<LongWritable, MapWritable> {
 
-        private SolrSplit split;
-        private SolrTableCursor cursor;
-        private int pos;
-        
-        String[] readColumns;
+    private SolrSplit split;
+    private SolrTableCursor cursor;
+    private int pos;
 
-        public SolrReader(String url, SolrSplit split, String[] readColumns, int numInputBufferRows) throws IOException {
-            this.split = split;
+    String[] readColumns;
 
-            this.readColumns = readColumns;
-            SolrTable table = new SolrTable(url);
-            if (numInputBufferRows > 0) {
-            	table.setNumInputBufferRows(numInputBufferRows);
-            }
-            cursor = table.findAll(readColumns, (int)split.getStart(), (int)split.getLength());
+    public SolrReader(String url, SolrSplit split, String[] readColumns, int numInputBufferRows) throws IOException {
+        this.split = split;
+
+        this.readColumns = readColumns;
+        SolrTable table = new SolrTable(url);
+        if (numInputBufferRows > 0) {
+            table.setNumInputBufferRows(numInputBufferRows);
         }
-        @Override
-        public void close() throws IOException {
+        cursor = table.findAll(readColumns, (int) split.getStart(), (int) split.getLength());
+    }
+
+    @Override
+    public void close() throws IOException {}
+
+    @Override
+    public LongWritable createKey() {
+        return new LongWritable();
+    }
+
+    @Override
+    public MapWritable createValue() {
+        return new MapWritable();
+    }
+
+    @Override
+    public long getPos() throws IOException {
+        return this.pos;
+    }
+
+    @Override
+    public float getProgress() throws IOException {
+        return split.getLength() > 0 ? pos / (float) split.getLength() : 1.0f;
+    }
+
+    @Override
+    public boolean next(LongWritable keyHolder, MapWritable valueHolder) throws IOException {
+        SolrDocument doc = cursor.nextDocument();
+        if (doc == null) {
+            return false;
         }
 
-        @Override
-        public LongWritable createKey() {
-            return new LongWritable();
-        }
+        keyHolder.set(pos++);
 
-        @Override
-        public MapWritable createValue() {
-            return new MapWritable();
+        for (int i = 0; i < readColumns.length; i++) {
+            String key = readColumns[i];
+            Object vObj = doc.getFieldValue(key);
+            Writable value = (vObj == null) ? NullWritable.get() : new Text(vObj.toString());
+            valueHolder.put(new Text(key), value);
         }
-
-        @Override
-        public long getPos() throws IOException {
-            return this.pos;
-        }
-
-        @Override
-        public float getProgress() throws IOException {
-            return split.getLength() > 0 ? pos / (float) split.getLength() : 1.0f;
-        }
-
-        @Override
-        public boolean next(LongWritable keyHolder, MapWritable valueHolder)
-                    throws IOException {
-        	SolrDocument doc = cursor.nextDocument();
-        	if (doc == null) {
-        		return false;
-        	}
-        	
-            keyHolder.set(pos++);
-            
-            for (int i = 0; i < readColumns.length; i++) {
-                String key = readColumns[i];
-                Object vObj = doc.getFieldValue(key);
-                Writable value = (vObj == null) ? NullWritable.get() : new Text(vObj.toString());
-                valueHolder.put(new Text(key), value);
-            }
-            return true;
-       }
+        return true;
+    }
 
 }
--- src/main/java/com/chimpler/hive/solr/SolrSerDe.java
+++ src/main/java/com/chimpler/hive/solr/SolrSerDe.java
@@ -7,7 +7,6 @@ import java.util.Properties;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hive.serde.Constants;
 import org.apache.hadoop.hive.serde2.AbstractSerDe;
 import org.apache.hadoop.hive.serde2.SerDeException;
 import org.apache.hadoop.hive.serde2.SerDeStats;
@@ -26,185 +25,163 @@ import org.apache.hadoop.io.Writable;
 import org.apache.log4j.Logger;
 
 public class SolrSerDe extends AbstractSerDe {
-    
-	static final String HIVE_TYPE_DOUBLE = "double";
-	static final String HIVE_TYPE_FLOAT = "float";
-	static final String HIVE_TYPE_BOOLEAN = "boolean";
-	static final String HIVE_TYPE_BIGINT = "bigint";
-	static final String HIVE_TYPE_TINYINT = "tinyint";
-	static final String HIVE_TYPE_SMALLINT = "smallint";
-	static final String HIVE_TYPE_INT = "int";
 
-	private final MapWritable cachedWritable = new MapWritable();
+    static final String HIVE_TYPE_DOUBLE = "double";
+    static final String HIVE_TYPE_FLOAT = "float";
+    static final String HIVE_TYPE_BOOLEAN = "boolean";
+    static final String HIVE_TYPE_BIGINT = "bigint";
+    static final String HIVE_TYPE_TINYINT = "tinyint";
+    static final String HIVE_TYPE_SMALLINT = "smallint";
+    static final String HIVE_TYPE_INT = "int";
 
-	private int fieldCount;
-	private StructObjectInspector objectInspector;
-	private List<String> columnNames;
-	String[] columnTypesArray;
-	private List<Object> row;
-	
-	Logger log = Logger.getLogger(SolrSerDe.class);
+    private final MapWritable cachedWritable = new MapWritable();
 
-	@Override
-	public void initialize(final Configuration conf, final Properties tbl)
-			throws SerDeException {
-		final String columnString = tbl
-				.getProperty(ConfigurationUtil.COLUMN_MAPPING);
-		if (StringUtils.isBlank(columnString)) {
-			throw new SerDeException("No column mapping found, use "
-					+ ConfigurationUtil.COLUMN_MAPPING);
-		}
-		final String[] columnNamesArray = ConfigurationUtil
-				.getAllColumns(columnString);
-		fieldCount = columnNamesArray.length;
-		columnNames = new ArrayList<String>(columnNamesArray.length);
-		columnNames.addAll(Arrays.asList(columnNamesArray));
-		
-		log.debug("column names in mongo collection: " + columnNames);
-		
-		String hiveColumnNameProperty = tbl.getProperty(Constants.LIST_COLUMNS);
-		List<String> hiveColumnNameArray = new ArrayList<String>();
-		
-		if (hiveColumnNameProperty != null && hiveColumnNameProperty.length() > 0) {
-			hiveColumnNameArray = Arrays.asList(hiveColumnNameProperty.split(","));
-		}
-		log.debug("column names in hive table: " + hiveColumnNameArray);
-		
-		String columnTypeProperty = tbl
-				.getProperty(Constants.LIST_COLUMN_TYPES);
-		// System.err.println("column types:" + columnTypeProperty);
-		columnTypesArray = columnTypeProperty.split(":");
-		log.debug("column types in hive table: " + columnTypesArray);
+    private int fieldCount;
+    private StructObjectInspector objectInspector;
+    private List<String> columnNames;
+    String[] columnTypesArray;
+    private List<Object> row;
 
-		final List<ObjectInspector> fieldOIs = new ArrayList<ObjectInspector>(
-				columnNamesArray.length);
-		for (int i = 0; i < columnNamesArray.length; i++) {
-			if (HIVE_TYPE_INT.equalsIgnoreCase(columnTypesArray[i])) {
-				fieldOIs
-						.add(PrimitiveObjectInspectorFactory.javaIntObjectInspector);
-			} else if (SolrSerDe.HIVE_TYPE_SMALLINT.equalsIgnoreCase(columnTypesArray[i])) {
-				fieldOIs
-						.add(PrimitiveObjectInspectorFactory.javaShortObjectInspector);
-			} else if (SolrSerDe.HIVE_TYPE_TINYINT.equalsIgnoreCase(columnTypesArray[i])) {
-				fieldOIs
-						.add(PrimitiveObjectInspectorFactory.javaByteObjectInspector);
-			} else if (SolrSerDe.HIVE_TYPE_BIGINT.equalsIgnoreCase(columnTypesArray[i])) {
-				fieldOIs
-						.add(PrimitiveObjectInspectorFactory.javaLongObjectInspector);
-			} else if (SolrSerDe.HIVE_TYPE_BOOLEAN.equalsIgnoreCase(columnTypesArray[i])) {
-				fieldOIs
-						.add(PrimitiveObjectInspectorFactory.javaBooleanObjectInspector);
-			} else if (SolrSerDe.HIVE_TYPE_FLOAT.equalsIgnoreCase(columnTypesArray[i])) {
-				fieldOIs
-						.add(PrimitiveObjectInspectorFactory.javaFloatObjectInspector);
-			} else if (SolrSerDe.HIVE_TYPE_DOUBLE.equalsIgnoreCase(columnTypesArray[i])) {
-				fieldOIs
-						.add(PrimitiveObjectInspectorFactory.javaDoubleObjectInspector);
-			} else {
-				// treat as string
-				fieldOIs
-						.add(PrimitiveObjectInspectorFactory.javaStringObjectInspector);
-			}
-		}
-		objectInspector = ObjectInspectorFactory
-				.getStandardStructObjectInspector(hiveColumnNameArray, fieldOIs);
-		row = new ArrayList<Object>(columnNamesArray.length);
-	}
+    Logger log = Logger.getLogger(SolrSerDe.class);
 
-	@Override
-	public Object deserialize(Writable wr) throws SerDeException {
-		if (!(wr instanceof MapWritable)) {
-			throw new SerDeException("Expected MapWritable, received "
-					+ wr.getClass().getName());
-		}
+    @Override
+    public void initialize(final Configuration conf, final Properties tbl) throws SerDeException {
+        final String columnString = tbl.getProperty(ConfigurationUtil.COLUMN_MAPPING);
+        if (StringUtils.isBlank(columnString)) {
+            throw new SerDeException("No column mapping found, use " + ConfigurationUtil.COLUMN_MAPPING);
+        }
+        final String[] columnNamesArray = ConfigurationUtil.getAllColumns(columnString);
+        fieldCount = columnNamesArray.length;
+        columnNames = new ArrayList<String>(columnNamesArray.length);
+        columnNames.addAll(Arrays.asList(columnNamesArray));
 
-		final MapWritable input = (MapWritable) wr;
-		final Text t = new Text();
-		row.clear();
+        log.debug("column names in mongo collection: " + columnNames);
 
-		for (int i = 0; i < fieldCount; i++) {
-			t.set(columnNames.get(i));
-			final Writable value = input.get(t);
-			if (value != null && !NullWritable.get().equals(value)) {
-				//parse as double to avoid NumberFormatException...
-				//TODO:need more test,especially for type 'bigint'
-				if (HIVE_TYPE_INT.equalsIgnoreCase(columnTypesArray[i])) {
-					row.add(Double.valueOf(value.toString()).intValue());
-				} else if (SolrSerDe.HIVE_TYPE_SMALLINT.equalsIgnoreCase(columnTypesArray[i])) {
-					row.add(Double.valueOf(value.toString()).shortValue());
-				} else if (SolrSerDe.HIVE_TYPE_TINYINT.equalsIgnoreCase(columnTypesArray[i])) {
-					row.add(Double.valueOf(value.toString()).byteValue());
-				} else if (SolrSerDe.HIVE_TYPE_BIGINT.equalsIgnoreCase(columnTypesArray[i])) {
-					row.add(Long.valueOf(value.toString()));
-				} else if (SolrSerDe.HIVE_TYPE_BOOLEAN.equalsIgnoreCase(columnTypesArray[i])) {
-					row.add(Boolean.valueOf(value.toString()));
-				} else if (SolrSerDe.HIVE_TYPE_FLOAT.equalsIgnoreCase(columnTypesArray[i])) {
-					row.add(Double.valueOf(value.toString()).floatValue());
-				} else if (SolrSerDe.HIVE_TYPE_DOUBLE.equalsIgnoreCase(columnTypesArray[i])) {
-					row.add(Double.valueOf(value.toString()));
-				} else {
-					row.add(value.toString());
-				}
-			} else {
-				row.add(null);
-			}
-		}
+        String hiveColumnNameProperty = tbl.getProperty(Constants.LIST_COLUMNS);
+        List<String> hiveColumnNameArray = new ArrayList<String>();
 
-		return row;
-	}
+        if (hiveColumnNameProperty != null && hiveColumnNameProperty.length() > 0) {
+            hiveColumnNameArray = Arrays.asList(hiveColumnNameProperty.split(","));
+        }
+        log.debug("column names in hive table: " + hiveColumnNameArray);
 
-	@Override
-	public ObjectInspector getObjectInspector() throws SerDeException {
-		return objectInspector;
-	}
+        String columnTypeProperty = tbl.getProperty(Constants.LIST_COLUMN_TYPES);
+        // System.err.println("column types:" + columnTypeProperty);
+        columnTypesArray = columnTypeProperty.split(":");
+        log.debug("column types in hive table: " + columnTypesArray);
 
-	@Override
-	public Class<? extends Writable> getSerializedClass() {
-		return MapWritable.class;
-	}
+        final List<ObjectInspector> fieldOIs = new ArrayList<ObjectInspector>(columnNamesArray.length);
+        for (int i = 0; i < columnNamesArray.length; i++) {
+            if (HIVE_TYPE_INT.equalsIgnoreCase(columnTypesArray[i])) {
+                fieldOIs.add(PrimitiveObjectInspectorFactory.javaIntObjectInspector);
+            } else if (SolrSerDe.HIVE_TYPE_SMALLINT.equalsIgnoreCase(columnTypesArray[i])) {
+                fieldOIs.add(PrimitiveObjectInspectorFactory.javaShortObjectInspector);
+            } else if (SolrSerDe.HIVE_TYPE_TINYINT.equalsIgnoreCase(columnTypesArray[i])) {
+                fieldOIs.add(PrimitiveObjectInspectorFactory.javaByteObjectInspector);
+            } else if (SolrSerDe.HIVE_TYPE_BIGINT.equalsIgnoreCase(columnTypesArray[i])) {
+                fieldOIs.add(PrimitiveObjectInspectorFactory.javaLongObjectInspector);
+            } else if (SolrSerDe.HIVE_TYPE_BOOLEAN.equalsIgnoreCase(columnTypesArray[i])) {
+                fieldOIs.add(PrimitiveObjectInspectorFactory.javaBooleanObjectInspector);
+            } else if (SolrSerDe.HIVE_TYPE_FLOAT.equalsIgnoreCase(columnTypesArray[i])) {
+                fieldOIs.add(PrimitiveObjectInspectorFactory.javaFloatObjectInspector);
+            } else if (SolrSerDe.HIVE_TYPE_DOUBLE.equalsIgnoreCase(columnTypesArray[i])) {
+                fieldOIs.add(PrimitiveObjectInspectorFactory.javaDoubleObjectInspector);
+            } else {
+                // treat as string
+                fieldOIs.add(PrimitiveObjectInspectorFactory.javaStringObjectInspector);
+            }
+        }
+        objectInspector = ObjectInspectorFactory.getStandardStructObjectInspector(hiveColumnNameArray, fieldOIs);
+        row = new ArrayList<Object>(columnNamesArray.length);
+    }
 
-	@Override
-	public Writable serialize(final Object obj, final ObjectInspector inspector)
-			throws SerDeException {
-		final StructObjectInspector structInspector = (StructObjectInspector) inspector;
-		final List<? extends StructField> fields = structInspector
-				.getAllStructFieldRefs();
-		if (fields.size() != columnNames.size()) {
-			throw new SerDeException(String.format(
-					"Required %d columns, received %d.", columnNames.size(),
-					fields.size()));
-		}
-		
-		cachedWritable.clear();
-		for (int c = 0; c < fieldCount; c++) {
-			StructField structField = fields.get(c);
-			if (structField != null) {
-				final Object field = structInspector.getStructFieldData(obj,
-						fields.get(c));
-				
-				//TODO:currently only support hive primitive type
-				final AbstractPrimitiveObjectInspector fieldOI = (AbstractPrimitiveObjectInspector)fields.get(c)
-						.getFieldObjectInspector();
+    @Override
+    public Object deserialize(Writable wr) throws SerDeException {
+        if (!(wr instanceof MapWritable)) {
+            throw new SerDeException("Expected MapWritable, received " + wr.getClass().getName());
+        }
 
-				Writable value = (Writable)fieldOI.getPrimitiveWritableObject(field);
-				
-				if (value == null) {
-					if(PrimitiveCategory.STRING.equals(fieldOI.getPrimitiveCategory())){
-						value = NullWritable.get();	
-						//value = new Text("");
-					}else{
-						//TODO: now all treat as number
-						value = new IntWritable(0);
-					}
-				}
-				cachedWritable.put(new Text(columnNames.get(c)), value);
-			}
-		}
-		return cachedWritable;
-	}
+        final MapWritable input = (MapWritable) wr;
+        final Text t = new Text();
+        row.clear();
 
-	@Override
-	public SerDeStats getSerDeStats() {
-		return new SerDeStats();
-	}
+        for (int i = 0; i < fieldCount; i++) {
+            t.set(columnNames.get(i));
+            final Writable value = input.get(t);
+            if (value != null && !NullWritable.get().equals(value)) {
+                // parse as double to avoid NumberFormatException...
+                // TODO:need more test,especially for type 'bigint'
+                if (HIVE_TYPE_INT.equalsIgnoreCase(columnTypesArray[i])) {
+                    row.add(Double.valueOf(value.toString()).intValue());
+                } else if (SolrSerDe.HIVE_TYPE_SMALLINT.equalsIgnoreCase(columnTypesArray[i])) {
+                    row.add(Double.valueOf(value.toString()).shortValue());
+                } else if (SolrSerDe.HIVE_TYPE_TINYINT.equalsIgnoreCase(columnTypesArray[i])) {
+                    row.add(Double.valueOf(value.toString()).byteValue());
+                } else if (SolrSerDe.HIVE_TYPE_BIGINT.equalsIgnoreCase(columnTypesArray[i])) {
+                    row.add(Long.valueOf(value.toString()));
+                } else if (SolrSerDe.HIVE_TYPE_BOOLEAN.equalsIgnoreCase(columnTypesArray[i])) {
+                    row.add(Boolean.valueOf(value.toString()));
+                } else if (SolrSerDe.HIVE_TYPE_FLOAT.equalsIgnoreCase(columnTypesArray[i])) {
+                    row.add(Double.valueOf(value.toString()).floatValue());
+                } else if (SolrSerDe.HIVE_TYPE_DOUBLE.equalsIgnoreCase(columnTypesArray[i])) {
+                    row.add(Double.valueOf(value.toString()));
+                } else {
+                    row.add(value.toString());
+                }
+            } else {
+                row.add(null);
+            }
+        }
+
+        return row;
+    }
+
+    @Override
+    public ObjectInspector getObjectInspector() throws SerDeException {
+        return objectInspector;
+    }
+
+    @Override
+    public Class<? extends Writable> getSerializedClass() {
+        return MapWritable.class;
+    }
+
+    @Override
+    public Writable serialize(final Object obj, final ObjectInspector inspector) throws SerDeException {
+        final StructObjectInspector structInspector = (StructObjectInspector) inspector;
+        final List<? extends StructField> fields = structInspector.getAllStructFieldRefs();
+        if (fields.size() != columnNames.size()) {
+            throw new SerDeException(String.format("Required %d columns, received %d.", columnNames.size(), fields.size()));
+        }
+
+        cachedWritable.clear();
+        for (int c = 0; c < fieldCount; c++) {
+            StructField structField = fields.get(c);
+            if (structField != null) {
+                final Object field = structInspector.getStructFieldData(obj, fields.get(c));
+
+                // TODO:currently only support hive primitive type
+                final AbstractPrimitiveObjectInspector fieldOI = (AbstractPrimitiveObjectInspector) fields.get(c).getFieldObjectInspector();
+
+                Writable value = (Writable) fieldOI.getPrimitiveWritableObject(field);
+
+                if (value == null) {
+                    if (PrimitiveCategory.STRING.equals(fieldOI.getPrimitiveCategory())) {
+                        value = NullWritable.get();
+                        // value = new Text("");
+                    } else {
+                        // TODO: now all treat as number
+                        value = new IntWritable(0);
+                    }
+                }
+                cachedWritable.put(new Text(columnNames.get(c)), value);
+            }
+        }
+        return cachedWritable;
+    }
+
+    @Override
+    public SerDeStats getSerDeStats() {
+        return new SerDeStats();
+    }
 }
